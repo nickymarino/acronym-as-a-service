0fff203e01dc2ac87eb72a442e6b71e3219e1ba0
Nicky Marino <nickymarino@icloud.com>
2021-04-17 23:43:32 -0500

    Add history handler

--- main.go
+++ main.go
@@ -17,7 +17,11 @@ type AcronymResponse struct {
 	Acronym string `json:"acronym"`
 }
 
-type AcronymHandler struct{}
+type History []AcronymResponse
+
+type AcronymHandler struct {
+	history *History
+}
 
 func (ah AcronymHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
@@ -37,6 +41,7 @@ func (ah AcronymHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			Acronym: acronym,
 		}
 
+		*ah.history = append(*ah.history, response)
 		json.NewEncoder(w).Encode(response)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
@@ -57,8 +62,33 @@ func acronymFrom(name string) string {
 	return acronym
 }
 
+type HistoryHandler struct {
+	history *History
+}
+
+func (hh HistoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	switch r.Method {
+	case http.MethodGet:
+		if len(*hh.history) == 0 {
+			http.Error(w, "Error: No acronym history found", http.StatusNotFound)
+			return
+		}
+
+		json.NewEncoder(w).Encode(hh.history)
+	default:
+		json.NewEncoder(w).Encode(hh.history)
+		// http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
+	var history History
+
 	mux := http.NewServeMux()
-	mux.Handle("/acronym", AcronymHandler{})
+	mux.Handle("/acronym", AcronymHandler{&history})
+	mux.Handle("/history", HistoryHandler{&history})
+
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
